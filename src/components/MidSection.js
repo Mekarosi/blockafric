d86356da1f891b8bda948174734d786e64e25186
Mekarosi <mekarosi_assa@yahoo.com>
2021-04-17 02:54:42 +0100

    More css style of mid section

--- src/components/MidSection.js
+++ src/components/MidSection.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import image5 from '../images/image5.JPG'
+import image6 from '../images/image6.JPG'
+import image7 from '../images/image7.JPG'
 
 class MidSection extends Component {
     render() {
@@ -33,6 +36,53 @@ class MidSection extends Component {
                     </div>
 
                 </div>
+                <div className="mid_convert_factor">
+                    <div>
+                        <span className="mid_convert_factor_text">AFYA</span> <span className="mid_convert_factor_number">0.00000001</span>
+                    </div>
+                    </div>
+                    <div className="send_receive">
+                        <div>
+                            <p className="send_text"><span className="send_text_item">SEND</span></p>
+                        </div>
+                        <div>
+                            <p className="receive_text"><span className="receive_text_item">RECEIVE</span></p>
+                        </div>
+                       
+                    </div>
+
+                    <div className="mid_sectionB">
+                        <div className="mid_sectionB_one">
+                            <p className="mid_sectionB_one_textB">Recent Activity</p>
+                        </div>
+                        <br/>
+
+                    </div>
+
+                    <ul>
+                        <li className="recent_activities_list">
+                            <span><img src={image5} className='' alt='battery' /></span>
+                            <p>HIV Blood Draw Test</p>
+                            <p>A100</p>
+
+                        </li>
+                        <li className="recent_activities_list">
+                            <span><img src={image6} className='' alt='battery' /></span>
+                            <p>TB Radiology XRay</p>
+                            <p>A30</p>
+                        </li>
+                        <li className="recent_activities_list">
+                        <span><img src={image7} className='' alt='battery' /></span>
+                            <p>Blood Pressure Check</p>
+                            <p>A10</p>
+                        </li>
+                    </ul>
+
+                    <div className="mid_sectionD"></div>
+                <br/>
+                
+
+
             </div>
         )
     }
